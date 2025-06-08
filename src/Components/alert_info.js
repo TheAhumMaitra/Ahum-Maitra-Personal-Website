2de24e361ab4b45dd53242d781c5a58474dd7f08
Your Name <theahummaitra@gmail.com>
2025-06-09 01:17:24 +0530

    fixed ' error in alert_info.js file

--- src/Components/alert_info.js
+++ src/Components/alert_info.js
@@ -2,7 +2,7 @@ import Alert from '@mui/material/Alert';
 
 export default function Alert_bug_info() {
     return(
-             <Alert severity="info" variant="filled">Hi there, User!  Sorry, there may be issues on this website.  I'm working through the issues, but it may take some time.  The entire website was created by me.  We appreciate you coming to this website, user!</Alert>
+             <Alert severity="info" variant="filled">Hi there, User!  Sorry, there may be issues on this website.  I am working through the issues, but it may take some time.  The entire website was created by me.  We appreciate you coming to this website, user!</Alert>
     )
 }
 
