1a3aeed11c08c1a240ec23dc3847bd74aed8ca88
Your Name <theahummaitra@gmail.com>
2025-06-09 01:14:25 +0530

    fixed ' error again

--- src/Components/hero.js
+++ src/Components/hero.js
@@ -4,17 +4,17 @@ import First_name_magic from './first_name_magic';
 
 export default function Hero_Section() {
     return (
-        <main>
+        <main className="px-4 sm:px-8">
             <Box
-                className="w-full h-[70vh] mt-4 border-4 flex flex-col gap-4 justify-center items-center
+                className="w-full h-[70vh] mt-4 border-4 flex flex-col gap-6 justify-center items-center
                            bg-white text-black border-green-700
-                           dark:bg-black dark:text-white dark:border-green-500"
+                           dark:bg-black dark:text-white dark:border-green-500 rounded-2xl shadow-md"
             >
-                <h2 className="font-bold text-4xl underline">Hello World!</h2>
-                <p className="text-center ">
+                <h2 className="font-bold text-4xl underline text-center">Hello World!</h2>
+                <p className="text-center max-w-2xl px-2">
                     Hello, everyone... I&apos;m Ahum. I&apos;m a programmer and this is my website! View my latest blogs
                     for free templates, projects, tutorials, and much more!{' '}
-                    <a href="././Blogs.js" className="underline text-green-600 dark:text-green-400 font-medium">
+                    <a href="/Blogs" className="underline text-green-600 dark:text-green-400 font-medium">
                         Latest blogs
                     </a>
                 </p>
@@ -23,3 +23,4 @@ export default function Hero_Section() {
         </main>
     );
 }
+
